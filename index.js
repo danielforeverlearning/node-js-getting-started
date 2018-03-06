e7eb8c0d39cdb74bf61da448ebefc0d25d54f78d
Max Lee <maxlee@pacrim.edu>
2018-03-05 19:24:15 -0800

    ejs test

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/ejstest', (req, res) => {
+      var ejs = require('ejs'),
+          people = ['geddy', 'neil', 'alex'],
+          html = ejs.render('<%= people.join(", "); %>', {people: people});
+      res.send(html);
+  })
   .get('/times', (req, res) => {
       var result = ''
       var times = process.env.TIMES || 5
